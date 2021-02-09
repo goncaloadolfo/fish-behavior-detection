ebd7d44ded63c1fb8d42afd225180148803c60d3
goncaloadolfo <goncaloadolfo20@gmail.com>
2021-02-09 15:51:19 +0000

    add ability to save bounding boxes information

--- trajectories_reader.py
+++ trajectories_reader.py
@@ -5,30 +5,42 @@ Fish entity; capability to read detections file.
 import numpy as np
 import cv2
 
+from collections import namedtuple
 from visualization import draw_trajectory
+
+
+BoundingBox = namedtuple("BoundingBox", ["width", "height"])
 
 
 class Fish:
     """
     Fish entity
     """
-    
+
     def __init__(self, fish_id):
         self.__fish_id = fish_id
         self.__trajectory = []
-    
-    def add_position(self, data_point):
+        self.__bounding_boxes_size = {}
+
+    def add_position(self, data_point, width, height):
         self.__trajectory.append(data_point)
+        self.__bounding_boxes_size[data_point[0]] = BoundingBox(width, height)
         return self
-    
+
     @property
     def fish_id(self):
-        return self.__fish_id    
-    
+        return self.__fish_id
+
     @property
     def trajectory(self):
-        return self.__trajectory    
-    
+        return self.__trajectory
+
+    def get_bounding_box_size(self, t):
+        return self.__bounding_boxes_size[t]
+
+    def get_position(self, t):
+        data_point = list(filter(lambda x: x[0] == t, self.__trajectory))
+        return data_point[0] if len(data_point) != 0 else None
 
 
 def produce_trajectories(detections_file):
@@ -41,24 +53,29 @@ def produce_trajectories(detections_file):
             # add the new detected positions to the fish
             for i in range(int(nr_fish)):
                 detection = fields[2+(i*5):2+(i*5)+5]
-                # centroid 
-                data_point = (frame, 
+                # centroid
+                data_point = (frame,
                               int((detection[0] + detection[2])/2),
                               int((detection[1] + detection[3])/2))
                 fish_id = detection[-1]
                 if fish_id in trajectories:
-                    trajectories[fish_id].add_position(data_point)
+                    trajectories[fish_id].add_position(
+                        data_point, abs(detection[0]-detection[2]), abs(detection[1]-detection[3]))
                 # new fish detected
-                else: 
-                    trajectories[fish_id] = Fish(fish_id).add_position(data_point)
-    return trajectories    
-    
+                else:
+                    trajectories[fish_id] = Fish(
+                        fish_id).add_position(data_point, abs(detection[0]-detection[2]), abs(detection[1]-detection[3]))
+    return trajectories
 
-if __name__ == "__main__":
+
+def read_trajectory_test():
     trajectories = produce_trajectories("../data/Dsc 0029-lowres_gt.txt")
     example_trajectory = list(trajectories.values())[0].trajectory
     print(example_trajectory)
     draw_trajectory(example_trajectory, (480, 720), (0, 0, 0))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    
+
+
+if __name__ == "__main__":
+    read_trajectory_test()
