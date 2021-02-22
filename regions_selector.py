7a926f74c86857fc5b713762d87aebfac860f10a
goncaloadolfo <goncaloadolfo20@gmail.com>
2021-02-22 17:03:43 +0000

    Add: script to help selecting image regions

---
+++ regions_selector.py
@@ -0,0 +1,220 @@
+"""
+Script to help defining geographic regions in the image plan.
+"""
+
+import sys
+import cv2
+import logging
+import random
+import json
+import os
+
+
+CONF_DIR_NAME = "conf"
+FRAME_NAME = "regions selector"
+regions_selector_logger = logging.getLogger(__name__)
+regions_selector_logger.setLevel(logging.INFO)
+regions_selector_logger.addHandler(logging.StreamHandler())
+
+
+class Rectangle():
+
+    def __init__(self):
+        self.__pt1 = None
+        self.__pt2 = None
+
+    @property
+    def pt1(self):
+        return self.__pt1
+
+    @property
+    def pt2(self):
+        return self.__pt2
+
+    @pt1.setter
+    def pt1(self, v):
+        self.__pt1 = v
+
+    @pt2.setter
+    def pt2(self, v):
+        self.__pt2 = v
+
+    @property
+    def centroid(self):
+        return (int((self.__pt1[0] + self.__pt2[0])/2), int((self.__pt1[1] + self.__pt2[1])/2))
+
+    def __contains__(self, v):
+        xs = sorted([self.__pt1[0], self.__pt2[0]])
+        ys = sorted([self.__pt1[1], self.__pt2[1]])
+        return v[0] >= xs[0] and v[0] <= xs[1] and v[1] >= ys[0] and v[1] <= ys[1]
+
+
+class RectangleEncoder(json.JSONEncoder):
+
+    def default(self, rectangle_obj):
+        return {
+            'pt1': rectangle_obj.pt1,
+            'pt2': rectangle_obj.pt2
+        }
+
+
+class Region():
+
+    def __init__(self, region_id, region_tag):
+        self.__region_id = region_id
+        self.__region_tag = region_tag
+        self.__region_rectangles = []
+        self.__color = (random.randint(0, 255),
+                        random.randint(0, 255),
+                        random.randint(0, 255))
+
+    @property
+    def region_id(self):
+        return self.__region_id
+
+    @property
+    def region_tag(self):
+        return self.__region_tag
+
+    @property
+    def rectangles(self):
+        return self.__region_rectangles
+
+    @property
+    def color(self):
+        return self.__color
+
+    def __contains__(self, v):
+        for rectangle in self.__region_rectangles:
+            if v in rectangle:
+                return True
+        return False
+
+    def __len__(self):
+        return len(self.__region_rectangles)
+
+    def __str__(self):
+        obj_str = f"- region {self.__region_id}\n"
+        for rect in self.__region_rectangles:
+            obj_str += f"- rect {rect.pt1}, {rect.pt2}\n"
+        return obj_str
+
+    def append(self, rectangle):
+        self.__region_rectangles.append(rectangle)
+
+    def remove(self, position):
+        for i in range(len(self.__region_rectangles)):
+            rect = self.__region_rectangles[i]
+            if position in rect:
+                del self.__region_rectangles[i]
+                return
+
+    def draw(self, frame):
+        for rect in self.__region_rectangles:
+            cv2.rectangle(frame, rect.pt1, rect.pt2, self.__color, 2)
+            cv2.putText(frame,
+                        str(self.__region_id),
+                        rect.centroid,
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        1,
+                        self.__color)
+
+
+class RegionEncoder(json.JSONEncoder):
+
+    def default(self, region):
+        rectEncoder = RectangleEncoder()
+        return {
+            'region-id': region.region_id,
+            'region-tag': region.region_tag,
+            'color': region.color,
+            'rectangles': [rectEncoder.encode(rect) for rect in region.rectangles]
+        }
+
+
+class RegionsSelector():
+
+    def __init__(self, input_file_path, output_filename, video_input_type=False):
+        self.__regions = {}
+        self.__output_filename = output_filename
+        self.__rectangle = None
+        # results frame
+        if video_input_type:
+            video_capture = cv2.VideoCapture(input_file_path)
+            _, self.__frame = video_capture.read()
+            video_capture.release()
+        else:
+            self.__frame = cv2.imread(input_file_path)
+        # create window and set mouse callback
+        cv2.namedWindow(FRAME_NAME)
+        cv2.setMouseCallback(FRAME_NAME, self.__mouse_callback)
+
+    def __mouse_callback(self, event, x, y, flags, param):
+        # left mouse click down - region selection
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.__rectangle = Rectangle()
+            self.__rectangle.pt1 = (x, y)
+        # left mouse click up - region selection
+        elif event == cv2.EVENT_LBUTTONUP:
+            self.__rectangle.pt2 = (x, y)
+            region_id = int(input("region number? \n > "))
+            # already exist
+            if region_id in self.__regions.keys():
+                self.__regions[region_id].append(self.__rectangle)
+            # doesn't exist
+            else:
+                region_tag = input("region tag? \n > ")
+                self.__regions[region_id] = Region(region_id, region_tag)
+                self.__regions[region_id].append(self.__rectangle)
+            self.__rectangle = None
+            regions_selector_logger.info("")
+            self.__print_regions()
+        # right click - region removal
+        elif event == cv2.EVENT_RBUTTONDOWN:
+            for region_id, region in self.__regions.items():
+                if (x, y) in region:
+                    region.remove((x, y))
+                    if len(region) == 0:
+                        del self.__regions[region_id]
+                        break
+            self.__print_regions()
+
+    def __print_regions(self):
+        if regions_selector_logger.level == logging.DEBUG:
+            for region in self.__regions.values():
+                print(region)
+
+    def __draw_regions(self):
+        frame_copy = self.__frame.copy()
+        for region in self.__regions.values():
+            region.draw(frame_copy)
+        return frame_copy
+
+    def __save_to_file(self):
+        if not os.path.isdir(CONF_DIR_NAME):
+            os.mkdir(CONF_DIR_NAME, mode=0o666)
+        conf_path = os.path.join(CONF_DIR_NAME,
+                                 self.__output_filename + ".conf")
+        with open(conf_path, "w") as f:
+            json.dump(RegionEncoder().encode(self.__regions), f)
+
+    def start(self):
+        regions_selector_logger.info("Select ROIs using left mouse click\n"
+                                     + "Remove rectangle using right click\n"
+                                     + "Press 's' to write into file\n")
+        while True:
+            cv2.imshow(FRAME_NAME, self.__draw_regions())
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("s"):
+                self.__save_to_file()
+                return
+
+
+def read_regions(config_path):
+    # todo
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    regions_selector_logger.setLevel(logging.DEBUG)
+    RegionsSelector(sys.argv[1], sys.argv[2], bool(sys.argv[3])).start()
