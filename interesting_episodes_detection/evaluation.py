80460f807fc3c8c357a5dfa8fb01f6cb5c171c21
goncaloadolfo <goncaloadolfo20@gmail.com>
2021-03-24 12:50:09 +0000

    Add: interesting episodes detection package

---
+++ interesting_episodes_detection/evaluation.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.metrics import (accuracy_score, f1_score, precision_score,
+                             recall_score, confusion_matrix)
+
+from trajectory_reader.visualization import simple_bar_chart
+
+
+METRICS = ["accuracy", "precision", "recall", "f1-score"]
+
+
+def evaluation_metrics(true_labels, predicted_labels):
+    return (
+        accuracy_score(true_labels, predicted_labels),
+        precision_score(true_labels, predicted_labels),
+        recall_score(true_labels, predicted_labels),
+        f1_score(true_labels, predicted_labels)
+    )
+
+
+def plot_metrics(values, x_values, classifier_label, width):
+    simple_bar_chart(plt.gca(), x_values, values,
+                     "Evaluation metrics", "value", "metric",
+                     label=classifier_label, width=width)
+
+
+def plot_confusion_matrix(true_labels, predicted_labels):
+    labels = np.unique(true_labels)
+    matrix = confusion_matrix(true_labels, predicted_labels,
+                              labels=labels)
+    plt.figure()
+    sns.heatmap(matrix, cmap="YlGnBu", xticklabels=labels, yticklabels=np.flip(labels),
+                vmin=0, vmax=len(true_labels), annot=True)
+    plt.title("Confusion matrix")
+    plt.ylabel("true labels")
+    plt.xlabel("predicted label")
+
+
+def holdout_prediction(model, samples, gt):
+    predictions = []
+
+    for i, sample in enumerate(samples):
+        # first sample
+        if i == 0:
+            x = samples[1:]
+            y = gt[1:]
+
+        # last sample
+        elif i == len(samples) - 1:
+            x = samples[:-1]
+            y = gt[:-1]
+
+        # middle samples
+        else:
+            x = np.vstack((samples[:i], samples[i+1:]))
+            y = gt[:i] + gt[i+1:]
+
+        # fit with the rest of the samples
+        model.fit(x, y)
+
+        # predict the label of the current sample
+        predictions.append(model.predict([sample])[0])
+
+    return predictions
---
+++ interesting_episodes_detection/naive_bayes.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from imblearn.over_sampling import SMOTE
+from pre_processing.pre_processing import CorrelatedVariablesRemoval, load_data
+from sklearn.decomposition import PCA
+from sklearn.naive_bayes import GaussianNB
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+from interesting_episodes_detection.evaluation import (METRICS,
+                                                       evaluation_metrics,
+                                                       holdout_prediction,
+                                                       plot_metrics)
+
+
+def compare_gnb_pipelines(species, balance=False):
+    x, y, _ = load_data("resources/datasets/v29-dataset1.csv",
+                        species)
+
+    if balance:
+        x, y = SMOTE().fit_resample(x, y)
+
+    pipelines = (
+        # gaussian NB with no pre processing
+        Pipeline([("gnb", GaussianNB())]),
+
+        # removing correlated variables
+        Pipeline([("cvr", CorrelatedVariablesRemoval(0.9)),
+                  ("gnb", GaussianNB())]),
+
+        # normalization only
+        Pipeline([("z-norm", StandardScaler()),
+                  ("gnb", GaussianNB())]),
+
+        # Applying PCA
+        Pipeline([("z-norm", StandardScaler()),
+                  ("pca", PCA(6)),
+                  ("gnb", GaussianNB())])
+    )
+
+    # plot results of each pipeline
+    nr_pipelines = len(pipelines)
+    nr_metrics = len(METRICS)
+    x_positions = np.arange(nr_metrics)
+    bar_width = 0.2
+
+    plt.figure()
+    for i, pipeline in enumerate(pipelines):
+        predictions = holdout_prediction(pipeline, x, y)
+        results = evaluation_metrics(y, predictions)
+
+        # bar positions
+        if i < nr_pipelines/2:
+            bar_positions = x_positions - bar_width * \
+                (nr_metrics/2 - i) + bar_width/2
+        else:
+            bar_positions = x_positions + bar_width * \
+                (i+1 - nr_metrics/2) - bar_width/2
+
+        plot_metrics(results, bar_positions,
+                     '+'.join(pipeline.named_steps.keys()),
+                     bar_width)
+
+    # plot settings
+    balance_str = "balanced" if balance else "not balanced"
+    plt.title(f"Evaluation of GNB pipelines " +
+              f"({','.join(list(species))}, {balance_str})")
+    plt.gca().set_xticks(x_positions)
+    plt.gca().set_xticklabels(METRICS)
+    plt.legend()
+    plt.grid()
+
+
+if __name__ == "__main__":
+    compare_gnb_pipelines(("shark", "manta-ray"))
+    compare_gnb_pipelines(("shark",))
+    compare_gnb_pipelines(("manta-ray",))
+
+    compare_gnb_pipelines(("shark", "manta-ray"), balance=True)
+    compare_gnb_pipelines(("shark",), balance=True)
+    compare_gnb_pipelines(("manta-ray",), balance=True)
+
+    plt.show()
