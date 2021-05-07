7e5d39495cefa0b4e982b93fb353c1b4fb9cceba
Gonçalo Adolfo <goncaloadolfo20@gmail.com>
2021-05-07 21:41:41 +0100

    Add: initialization of transition and covariance parameters; identification of active vectors

--- fish-behavior-detection/anomaly_detection/switching_vector_fields.py
+++ fish-behavior-detection/anomaly_detection/switching_vector_fields.py
@@ -10,24 +10,60 @@ from sklearn.cluster import KMeans
 from trajectory_reader.trajectories_reader import read_fishes
 
 
-def initialize_vectors(grid, k):
+def initialize_parameters(grid, k):
     kmeans = KMeans(n_clusters=k)
 
     for node in grid:
-        node_vectors = node.all_motion_vectors
+        node_vectors = np.array(node.all_motion_vectors)
 
         if len(node_vectors) > 0:
             kmeans.fit(np.array(node_vectors))
             node.clusters = kmeans.labels_
 
             labels, counts = np.unique(node.clusters, return_counts=True)
+            total_node_vectors = np.sum(counts)
             label_count = list(zip(labels, counts))
-            label_count.sort(key=lambda x: x[1])
+            label_count.sort(key=lambda x: x[1], reverse=True)
 
             core_vectors = []
-            for label, _ in label_count:
+            transition_matrix = []
+            vectors_variance = []
+            for label, count in label_count:
                 core_vectors.append(kmeans.cluster_centers_[label])
+                initial_prob = count / total_node_vectors
+                transition_matrix.append([initial_prob] * k)
+
+                label_samples = node_vectors[node.clusters == label]
+                vector_variance = np.var(label_samples, axis=0)
+                vectors_variance.append([
+                    [vector_variance[0], 0],
+                    [0, vector_variance[1]]
+                ])
+
             node.core_vectors = core_vectors
+            node.transition_matrix = np.array(transition_matrix).T
+            node.vectors_variance = np.array(vectors_variance)
+
+
+def active_vectors(grid, trajectory):
+    active_vectors = []
+    for i in range(1, len(trajectory)):
+        motion_vector = [trajectory[i][1] - trajectory[i-1][1],
+                         trajectory[i][2] - trajectory[i-1][2]]
+        for node in grid:
+            if (trajectory[i][1], trajectory[i][2]) in node:
+                active_vectors.append(
+                    (node.node_id, most_likely_vector(node, motion_vector))
+                )
+                break
+
+    return active_vectors
+
+
+def most_likely_vector(node, motion_vector):
+    diffs = np.array(motion_vector) - node.core_vectors
+    norms = np.linalg.norm(diffs, axis=1)
+    return np.argmin(norms)
 
 
 def set_motion_vectors(fishes_file, grid):
@@ -46,6 +82,7 @@ def save_motion_vectors(grid, trajectory):
         for node in grid:
             if (trajectory[i][1], trajectory[i][2]) in node:
                 node.all_motion_vectors.append(motion_vector)
+                break
 
 
 def draw_motion_vectors(grid, frame_shape, k):
@@ -99,13 +136,20 @@ def generate_colors(k, clusters):
 
 class GridNode:
 
-    def __init__(self, x_limits, y_limits):
+    def __init__(self, node_id, x_limits, y_limits):
+        self.__node_id = node_id
         self.__x_limits = x_limits
         self.__y_limits = y_limits
 
         self.__all_motion_vectors = []
         self.__clusters = []
         self.__core_vectors = []
+        self.__transition_matrix = None
+        self.__vectors_variance = None
+
+    @property
+    def node_id(self):
+        return self.__node_id
 
     @property
     def x_limits(self):
@@ -127,6 +171,14 @@ class GridNode:
     def core_vectors(self):
         return self.__core_vectors
 
+    @property
+    def transition_matrix(self):
+        return self.__transition_matrix
+
+    @property
+    def vectors_variance(self):
+        return self.__vectors_variance
+
     @core_vectors.setter
     def core_vectors(self, value):
         self.__core_vectors = value
@@ -135,9 +187,26 @@ class GridNode:
     def clusters(self, value):
         self.__clusters = value
 
+    @transition_matrix.setter
+    def transition_matrix(self, value):
+        self.__transition_matrix = value
+
+    @vectors_variance.setter
+    def vectors_variance(self, value):
+        self.__vectors_variance = value
+
     def __contains__(self, value):
         return self.__x_limits[0] <= value[0] <= self.__x_limits[1] \
             and self.__y_limits[0] <= value[1] <= self.__y_limits[1]
+
+    def __str__(self):
+        result = ""
+        result += f"X Limits: {self.__x_limits}\n"
+        result += f"Y Limits: {self.__y_limits}\n"
+        result += f"Core Vectors:\n{self.__core_vectors}\n"
+        result += f"Transition Matrix:\n{self.__transition_matrix}\n"
+        result += f"Vectors Variance:\n{self.__vectors_variance}"
+        return result
 
 
 def create_grid(nr_nodes, frame_shape):
@@ -149,6 +218,7 @@ def create_grid(nr_nodes, frame_shape):
     for i in range(n):
         for j in range(n):
             nodes.append(GridNode(
+                i + j,
                 (x_pace*i, x_pace*(i+1)),
                 (y_pace*j, y_pace*(j+1))
             ))
@@ -163,12 +233,15 @@ if __name__ == "__main__":
     grid = create_grid(49, frame_shape)
 
     set_motion_vectors(fishes, grid)
-    initialize_vectors(grid, k)
+    initialize_parameters(grid, k)
 
     vector_frames = draw_motion_vectors(grid, frame_shape, k)
     for i in range(k):
         cv2.imshow(f"motion frame {i}", vector_frames[i])
-    visualize_clusters(random.choice(grid))
+
+    example_node = random.choice(grid)
+    visualize_clusters(example_node)
+    print(example_node)
 
     plt.show()
     cv2.destroyAllWindows()
