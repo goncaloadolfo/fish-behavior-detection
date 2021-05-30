9d90e5fc1b57a5ea7b3932efc124422a04e36af8
goncaloadolfo <goncaloadolfo20@gmail.com>
2021-05-30 11:14:26 +0100

    Add: conclusion of expectation module, forward-backward procedure

--- src/fish-behavior-detection/anomaly_detection/switching_model/model_expectation.py
+++ src/fish-behavior-detection/anomaly_detection/switching_model/model_expectation.py
@@ -49,3 +49,59 @@ def complete_log_likelihood(grid, trajectories):
         complete_likelihood += np.log(joint_probability(grid, trajectory))
     
     return complete_likelihood
+
+
+def normalize_vector(vector):
+    return vector / np.sum(vector)
+
+
+def calculate_forward_probabilities(grid, trajectory):
+    forward_probabilities = np.empty((grid.nr_fields, len(trajectory) + 1))
+    forward_probabilities[:, 0] = [1 / grid.nr_fields] * grid.nr_fields
+    
+    for i in range(len(trajectory)):
+        t, x, y = trajectory[i]
+        node_indexes = identify_position_node(grid, (x, y))
+        node = grid.grid_matrix[node_indexes[1]][node_indexes[0]]
+        node_transition_matrix = node.transition_matrix
+        
+        previous_probs = forward_probabilities[:, i].reshape((grid.nr_fields, 1)) 
+        aux = np.dot(node_transition_matrix, previous_probs).flatten()
+        current_probabilities = normalize_vector(aux)
+        forward_probabilities[:, i+1] = current_probabilities
+    
+    return forward_probabilities[:, 1:]
+        
+        
+def calculate_backward_probabilities(grid, trajectory):
+    backward_probabilities = np.empty((grid.nr_fields, len(trajectory) + 1))
+    backward_probabilities[:, -1] = [1.0] * grid.nr_fields
+    
+    for i in range(len(trajectory) - 1, -1, -1):
+        t, x, y = trajectory[i]
+        node_indexes = identify_position_node(grid, (x, y))
+        node = grid.grid_matrix[node_indexes[1]][node_indexes[0]]
+        node_transition_matrix = node.transition_matrix
+
+        previous_probs = backward_probabilities[:, i + 1].reshape((grid.nr_fields, 1)) 
+        aux = np.dot(node_transition_matrix, previous_probs).flatten()
+        current_probabilities = normalize_vector(aux)
+        backward_probabilities[:, i] = current_probabilities
+    
+    return backward_probabilities[:, :-1]
+
+
+def forward_backward_procedure(grid, trajectory):
+    forward_probs = calculate_forward_probabilities(grid, trajectory)
+    backward_probs = calculate_backward_probabilities(grid, trajectory)
+    smooth_probs = forward_probs * backward_probs
+    
+    vectors_sums = np.sum(smooth_probs, axis=0)
+    return smooth_probs / vectors_sums    
+ 
+ 
+def calculate_transition_probability(probs_fields_t, t, field1, field2):
+    prob_field1 = probs_fields_t[field1][t-1]
+    prob_field2 = probs_fields_t[field2][t]
+    return prob_field1 * prob_field2
+    
