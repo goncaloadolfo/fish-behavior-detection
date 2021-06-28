edbc52ec18b2a53660cc85b16e6d1e53eebfccde
goncaloadolfo <goncaloadolfo20@gmail.com>
2021-06-28 23:04:06 +0100

    Refractor and clean up code

--- src/behavior-detection-module/rule_based/feeding_motion_level.py
+++ src/behavior-detection-module/rule_based/feeding_motion_level.py
@@ -1,6 +1,7 @@
 import sys
 
 import cv2
+import seaborn
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,28 +18,35 @@ def active_pixels(frame_t1, frame_t2, motion_thr, return_frame=False):
     return np.sum(motion_frame == 255) if not return_frame else np.sum(motion_frame == 255), motion_frame
 
 
-def extract_feeding_warnings(video_capture, motion_thr, feeding_thr, duration, return_time_series=False):
-    motion_time_series = []
-    feeding_warnings = []
-    feeding_flag = False
-    feeding_duration = 0
-
-    t = 1
-    _, previous_frame = video_capture.read()
+def calculate_motion_time_series(video_capture, motion_thr):
+    time_series = []
     total_frames = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    _, previous_frame = video_capture.read()
+    t = 0
 
     while True:
         print(f"processing frame {t}/{total_frames}")
         _, current_frame = video_capture.read()
         if current_frame is None:
-            if feeding_flag:
-                feeding_warnings.append((t - 1 - feeding_duration, t - 1))
             break
 
         nr_active_pixels, _ = active_pixels(previous_frame,
                                             current_frame, motion_thr)
-        if return_time_series:
-            motion_time_series.append(nr_active_pixels)
+        time_series.append(nr_active_pixels)
+
+        previous_frame = current_frame
+        t += 1
+
+    return time_series
+
+
+def extract_feeding_warnings(motion_time_series, feeding_thr, duration):
+    feeding_warnings = []
+    feeding_flag = False
+    feeding_duration = 0
+
+    for t in range(len(motion_time_series)):
+        nr_active_pixels = motion_time_series[t]
 
         if nr_active_pixels >= feeding_thr:
             feeding_duration += 1
@@ -46,15 +54,17 @@ def extract_feeding_warnings(video_capture, motion_thr, feeding_thr, duration, r
 
         elif feeding_flag and feeding_duration >= duration:
             feeding_warnings.append((t - 1 - feeding_duration, t - 1))
-            _reset_status(feeding_flag, feeding_duration)
+            feeding_duration = 0
+            feeding_flag = False
 
         else:
-            _reset_status(feeding_flag, feeding_duration)
+            feeding_duration = 0
+            feeding_flag = False
 
-        previous_frame = current_frame
-        t += 1
+        if t == len(motion_time_series) - 1 and feeding_flag:
+            feeding_warnings.append((t - feeding_duration, t))
 
-    return feeding_warnings if not return_time_series else feeding_warnings, motion_time_series
+    return feeding_warnings
 
 
 def tune_motion_thr(video_capture, motion_thrs):
@@ -62,8 +72,8 @@ def tune_motion_thr(video_capture, motion_thrs):
 
     for motion_thr in motion_thrs:
         video_capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        _, motion_time_series = extract_feeding_warnings(video_capture, motion_thr, sys.maxsize,
-                                                         sys.maxsize, return_time_series=True)
+        motion_time_series = calculate_motion_time_series(video_capture,
+                                                          motion_thr)
         motion_time_series = motion_time_series[::30]
         TrajectoryFeatureExtraction.exponential_sliding_average(motion_time_series, 24,
                                                                 exponential_weights(24, 0.1,
@@ -80,30 +90,46 @@ def tune_motion_thr(video_capture, motion_thrs):
 
 def get_example_motion_frames(video_capture, motion_thr, feeding_warnings):
     first_feeding_warning = feeding_warnings[0]
-    t_normal = int(first_feeding_warning[0]/2)
-    t_feeding = int((first_feeding_warning[1] + first_feeding_warning[0])/2)
+    t_normal = int(first_feeding_warning[0]/2 * 30)
+    t_feeding = int(
+        (first_feeding_warning[1] + first_feeding_warning[0])/2 * 30)
     return (_calculate_diff_frame(video_capture, motion_thr, t_normal),
             _calculate_diff_frame(video_capture, motion_thr, t_feeding))
 
 
 def evaluate_motion_method(video_capture, motion_thr, feeding_thr, duration, ground_truth):
     confusion_matrix = np.zeros((2, 2), dtype=int)
-    feeding_warnings = extract_feeding_warnings(video_capture, motion_thr,
-                                                feeding_thr, duration)
+    motion_time_series = calculate_motion_time_series(video_capture,
+                                                      motion_thr)
+    y = motion_time_series[::30]
+    smoothed_y = y.copy()
+    TrajectoryFeatureExtraction.exponential_sliding_average(smoothed_y, 30,
+                                                            exponential_weights(30, 0.1,
+                                                                                forward_only=True))
+    feeding_warnings = extract_feeding_warnings(smoothed_y, feeding_thr,
+                                                duration)
     total_frames = video_capture.get(cv2.CAP_PROP_FRAME_COUNT)
 
-    for t in range(1, total_frames):
-        predicted_class = _get_predicted_class(t, feeding_warnings)
-        true_class = _get_true_class(t, ground_truth)
+    for t in range(len(smoothed_y)):
+        predicted_class = _get_predicted_class(t*30, feeding_warnings)
+        true_class = _get_true_class(t*30, ground_truth)
         confusion_matrix[predicted_class][true_class] += 1
 
     return confusion_matrix
 
 
-def analyze_training_results(video_capture, motion_thr, feeding_thr, duration, show_frames=False):
-    feeding_warnings, active_pixels_time_series = extract_feeding_warnings(video_capture, motion_thr,
-                                                                           feeding_thr, duration,
-                                                                           return_time_series=True)
+def analyze_training_results(video_capture, motion_thr, feeding_thr, duration,
+                             show_frames=False, show_feeding_results=False):
+    motion_time_series = calculate_motion_time_series(video_capture,
+                                                      motion_thr)
+    y = motion_time_series[::30]
+    smoothed_y = y.copy()
+    TrajectoryFeatureExtraction.exponential_sliding_average(smoothed_y, 30,
+                                                            exponential_weights(30, 0.1,
+                                                                                forward_only=True))
+    feeding_warnings = extract_feeding_warnings(smoothed_y, feeding_thr,
+                                                duration)
+
     x = np.arange(1, video_capture.get(cv2.CAP_PROP_FRAME_COUNT)) / 30.0
     plt.figure()
     plt.title("Motion Variance")
@@ -111,11 +137,6 @@ def analyze_training_results(video_capture, motion_thr, feeding_thr, duration, s
     plt.ylabel("motion level")
     x = x[::30]
 
-    y = active_pixels_time_series[::30]
-    smoothed_y = y.copy()
-    TrajectoryFeatureExtraction.exponential_sliding_average(smoothed_y, 24,
-                                                            exponential_weights(24, 0.1,
-                                                                                forward_only=True))
     plt.plot(x, y, label="original")
     plt.plot(x, smoothed_y, label="smooth")
     plt.legend()
@@ -126,16 +147,26 @@ def analyze_training_results(video_capture, motion_thr, feeding_thr, duration, s
     plt.xlabel("#active pixels")
     plt.hist(smoothed_y)
 
+    if show_feeding_results:
+        plt.figure()
+        plt.title("Feeding Periods")
+        plt.ylabel("motion level")
+        plt.xlabel("t(s)")
+        plt.plot(x, smoothed_y)
+
+        for t_initial, t_final in feeding_warnings:
+            start_index = int(t_initial)
+            end_index = int(t_final)
+            plt.plot(x[start_index: end_index+1],
+                     smoothed_y[start_index: end_index+1],
+                     label="feeding period")
+        plt.legend()
+
     if show_frames:
         normal_motion_frame, feeding_motion_frame = get_example_motion_frames(video_capture,
                                                                               motion_thr, feeding_warnings)
         cv2.imshow("normal motion frame", normal_motion_frame)
         cv2.imshow("feeding motion frame", feeding_motion_frame)
-
-
-def _reset_status(feeding_flag, feeding_duration):
-    feeding_duration = 0
-    feeding_flag = False
 
 
 def _get_predicted_class(frame, predicted_warnings):
@@ -164,13 +195,27 @@ def _calculate_diff_frame(video_capture, motion_thr, t):
     video_capture.set(cv2.CAP_PROP_POS_FRAMES, t)
     _, frame_t1 = video_capture.read()
     _, frame_t2 = video_capture.read()
-    return active_pixels(frame_t1, frame_t2, motion_thr, return_frame=True)
+    frame = active_pixels(frame_t1, frame_t2, motion_thr, return_frame=True)[1]
+    return cv2.resize(frame, (1280, 720))
 
 
 if __name__ == "__main__":
     video_capture = cv2.VideoCapture("resources/videos/feeding-v1-trim.mp4")
+    video_test1 = cv2.VideoCapture("resources/videos/feeding-v1-trim2.mp4")
+
     # tune_motion_thr(video_capture, [40, 50, 60, 70])
+
     # video_capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
-    analyze_training_results(video_capture, 40, sys.maxsize, sys.maxsize)
+    # analyze_training_results(video_capture, 40, 625_000, 20,
+    #                          show_frames=True, show_feeding_results=True)
+
+    results = evaluate_motion_method(video_test1, 40, 650_000, 20, [])
+    plt.figure()
+    plt.title("Results Test Video 1")
+    seaborn.heatmap(results, ax=plt.gca(), vmin=0,
+                    vmax=np.sum(results), annot=True)
+    print(results)
+
     plt.show()
     video_capture.release()
+    video_test1.release()
